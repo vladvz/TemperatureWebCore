4b00573f8f84d3837b36fb2decf36d00cc457cf1
Vladimir Zemskov <vladimir.zemskov@unodc.org>
2017-11-24 13:30:33 +0100

    Add missing schema sqls

---
+++ TemperatureWebCore/SQL/Table_Measure.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Measure (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Time datetime NOT NULL,
+  Temperature double DEFAULT NULL,
+  PRIMARY KEY (Id)
+) ENGINE=InnoDB AUTO_INCREMENT=8597 DEFAULT CHARSET=utf8mb4
